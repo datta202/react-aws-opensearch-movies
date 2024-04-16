16870482942c91df02b0f9553936b62dd96429e6
Datta Gaikwad <datta@avantinsights.com>
2024-04-16 09:17:00 +0530

    fixed cosmetic

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,7 @@ import coverImg from '../assets/cover.avif';
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <div>
+  return <div style={{ maxWidth: 1000, margin: "0 auto", textAlign: "center" }}>
     <Link to="projects">Go to Projects</Link>
     <h2>Website Under Construction</h2>
     <img src={coverImg} alt="" />
